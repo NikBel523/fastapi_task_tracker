d54da81e7b32259bcdd55b6859282545c89eb66f
NikBel523 <belyakov.nkt@gmail.com>
2024-10-15 16:49:33 +0300

    feat: add PostgresQL to the project

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Путь к базе данных
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Подключение к базе данных
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Базовый класс для всех моделей
